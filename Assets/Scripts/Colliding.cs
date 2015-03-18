adf810467f81b6209575219d5966a7d6774ec4be
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-03-18 13:00:04 -0400

    isThereAVectorCrossingWith implemented. Added the function segmentsIntersect (not implemented)

--- Assets/Scripts/Colliding.cs
+++ Assets/Scripts/Colliding.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Colliding : MonoBehaviour {
+public class Colliding : MonoBehaviour
+{
 
     protected bool canBeCollided = false;
 
@@ -51,7 +52,7 @@ public class Colliding : MonoBehaviour {
             //Debug.Log(dist_centers + " | " + (other_radius + my_radius) + " | " + other_radius + " | " + my_radius + " | " + other.position + " | " + transform.position);
             return false;
         }
-        
+
         //Rectangles
         if (other.position.x < transform.position.x + my_size.x
             && transform.position.x < other.position.x + other_size.x
@@ -68,12 +69,46 @@ public class Colliding : MonoBehaviour {
 
     bool isThereAVectorCrossingWith(Transform other)
     {
-        //TODO
-        return true;
+        Vector3[] other_vertices = other.GetComponent<Sprite>().mMesh.vertices;
+        Vector3[] my_vertices = GetComponent<Sprite>().mMesh.vertices;
+        int[] other_indices = other.GetComponent<Sprite>().mIndices;
+        int[] my_indices = GetComponent<Sprite>().mIndices;
+
+        for (int other_triangle_index = 0; other_triangle_index < other_indices.Length / 3; ++other_triangle_index)
+        {
+            //Cast Vector3 in Vector2. Contains p0, p1, p2 and p0 again. So that p0 -> p1 is a first segment, p1 -> p2 a second one, and p2 -> p0 the third one
+            Vector2[] other_current_tri_points = { other_vertices[3 * other_triangle_index], other_vertices[3 * other_triangle_index + 1], other_vertices[3 * other_triangle_index + 2], other_vertices[3 * other_triangle_index] };
+            for (int my_triangle_index = 0; my_triangle_index < my_indices.Length / 3; ++my_triangle_index)
+            {
+                //Cast Vector3 in Vector2. Contains p0, p1, p2 and p0 again. So that p0 -> p1 is a first segment, p1 -> p2 a second one, and p2 -> p0 the third one
+                Vector2[] my_current_tri_points = { my_vertices[3 * my_triangle_index], my_vertices[3 * my_triangle_index + 1], my_vertices[3 * my_triangle_index + 2], my_vertices[3 * my_triangle_index] };
+
+                //Check combination of all segments unless collision is found
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; i < 3; ++j)
+                    {
+                        if (segmentsIntersect(other_current_tri_points[i], other_current_tri_points[i + 1], my_current_tri_points[j], my_current_tri_points[j + 1]))
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return false;
     }
 
     float calcDist(float dx, float dy)
     {
         return Mathf.Sqrt(dx * dx + dy * dy);
+    }
+
+    bool segmentsIntersect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4)
+    {
+        //TODO
+        return true;
     }
 }
